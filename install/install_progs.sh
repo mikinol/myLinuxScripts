f141e330045503efcce3f3c4d4c10539ceecfa64
mikinol <sm44aksdmiki13877kfh@gmail.com>
2024-08-10 08:53:01 +0300

    Добавлена установка VsCode

--- install/install_progs.sh
+++ install/install_progs.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")" || exit
 
-echo "Хотите ли вы установить Discord? (Д/Н)"
+echo "Хотите ли вы установить Discord? (Д/н)"
 read -r ans
 
 case $ans in
@@ -12,5 +12,18 @@ case $ans in
         ;;
     * )
         echo "Установка Discord отменена, вы всё ещё можете установить Discord выполнив $(dirname "$0")/progs/discord.sh"
+        ;;
+esac
+
+echo "Хотите ли вы установить VsCode (Требуются установленные репозитории из install_repos.sh)? (Д/н)"
+read -r ans
+
+case $ans in
+    [YyДд]* )
+        echo "Установка VsCode начинается."
+        sudo apt install code
+        ;;
+    * )
+        echo "Установка Discord отменена, вы всё ещё можете установить Discord выполнив \"sudo apt install code\""
         ;;
 esac

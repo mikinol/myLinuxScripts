147ff444ed1fe2ab446ce3bc76e6b4f712eb3297
mikinol <sm44aksdmiki13877kfh@gmail.com>
2024-10-19 14:11:34 +0300

    Исправлена ошибка

--- install/repos/kdenlive_ppa.sh
+++ install/repos/kdenlive_ppa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Добавление ppa репозитория fastfetch"
+echo "Добавление ppa репозитория kdenlive"
 sudo add-apt-repository ppa:kdenlive/kdenlive-stable
 echo "Добавление репозитория завершено"
 

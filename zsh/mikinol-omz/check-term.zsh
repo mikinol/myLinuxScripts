12408c01f0e93060305fffb28223b81eec7b9ce7
mikinol beeland <sm44aksdmiki13877kfh@gmail.com>
2025-06-18 00:34:02 +0300

    Remove ненужное сообщение

--- zsh/mikinol-omz/check-term.zsh
+++ zsh/mikinol-omz/check-term.zsh
@@ -1,4 +1,4 @@
 if ! infocmp "$TERM" >/dev/null 2>&1; then
-  echo "$TERM is not supported on this server, using xterm-256color"
+  #echo "$TERM is not supported on this server, using xterm-256color"
   export TERM="xterm-256color"
 fi

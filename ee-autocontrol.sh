c105a9aa6bd8297e436411803ee9b41baaccb597
mikinol <sm44aksdmiki13877kfh@gmail.com>
2025-08-02 16:26:00 +0300

    ee-autocontrol

---
+++ ee-autocontrol.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+CHECK_INTERVAL=5
+IDLE_THRESHOLD=30  # секунд
+IDLE_COUNTER=0
+
+while true; do
+
+  IS_MUTED=$(pactl get-source-mute @DEFAULT_SOURCE@ | awk '{ print $2 }')
+
+  if [[ "$IS_MUTED" == "yes" ]]; then
+    IDLE_COUNTER=$((IDLE_COUNTER + CHECK_INTERVAL))
+  else
+    IDLE_COUNTER=0
+  fi
+
+  if [[ $IDLE_COUNTER -ge $IDLE_THRESHOLD ]]; then
+    systemctl --user is-active easyeffects && systemctl --user stop easyeffects
+  else
+    ! systemctl --user is-active easyeffects && systemctl --user start easyeffects
+  fi
+
+  sleep $CHECK_INTERVAL
+done
+

dd5adce2a55406fddbcbd905d73764cbd803967e
mikinol <sm44aksdmiki13877kfh@gmail.com>
2026-02-25 12:59:06 +0300

    Испрввил проверку terminfo на Termux

--- zsh/mikinol-omz/check-term.zsh
+++ zsh/mikinol-omz/check-term.zsh
@@ -1,3 +1,7 @@
+if [[ -n "$PREFIX" && "$PREFIX" == *termux* ]]; then
+  return
+fi
+
 if ! infocmp "$TERM" >/dev/null 2>&1; then
   echo "$TERM is not supported on this server, using xterm-256color"
   export TERM="xterm-256color"

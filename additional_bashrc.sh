7d3343f6c8556abc295205438ac2d4526ed466aa
mikinol <sm44aksdmiki13877kfh@gmail.com>
2026-04-15 10:48:35 +0300

    Добавил toupper и tolower

--- additional_bashrc.sh
+++ additional_bashrc.sh
@@ -1,3 +1,8 @@
 alias urlencode='python3 -c "import sys, urllib.parse; print(urllib.parse.quote(sys.stdin.read().strip()))"'
 alias urldecode='python3 -c "import sys, urllib.parse; print(urllib.parse.unquote(sys.stdin.read().strip()))"'
 alias transliterate='uconv -x "Any-Latin"'
+
+# shellcheck disable=SC2142
+alias toupper='awk "{print toupper(\$0)}"'
+# shellcheck disable=SC2142
+alias tolower='awk "{print tolower(\$0)}"'

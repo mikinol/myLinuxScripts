a6cb097b4795c940af5c15b24342420b3844a248
mikinol <sm44aksdmiki13877kfh@gmail.com>
2026-04-21 16:49:08 +0300

    Добавил ещё больше конченого функционала и оптимизаций в password_gen

--- src/password_gen.c
+++ src/password_gen.c
@@ -13,13 +13,31 @@ const char letters[53] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const char numbers[10] = "0123456789";
 const char symbols[25] = "!@#$%^&*()_+[]{}|;:',.<>?";
 
+const char *dicts[3] = {
+    "0123456789ABCDEF", // HEX с большими  буквами
+    "0123456789abcdef", // HEX с маленькими буквами
+};
+
+enum dictionaries { HEX_UPPER = 0, HEX_LOWER = 1, IPv6 = 1, UNKNOWN = -1 };
+
+enum dictionaries parse_dict(const char *arg) {
+  if (strcmp(arg, "hexupper") == 0)
+    return HEX_UPPER;
+  if (strcmp(arg, "hexlower") == 0)
+    return HEX_LOWER;
+  return UNKNOWN;
+}
+
 int main(int argc, char **argv) {
   if (argc < 3) {
     fprintf(stderr,
             "Недостаточно агрументов, password_gen <длинна пароля> <количество "
             "паролей> (+s включает специальные символы) (-c выключает символы) "
             "(-n выключает цифры)\nИли можно password_gen <длинна пароля> "
-            "<количество паролей> <список доступных символов>");
+            "<количество паролей> <список доступных символов>\nИ ещё можно "
+            "использовать другие словари password_gen <длинна пароля> "
+            "<количество паролей> @<название словаря>\nСловари:\n- hexupper — "
+            "HEX с большими буквами\n- hexlower — HEX с маленькими буквами");
     return 1;
   }
 
@@ -71,9 +89,20 @@ int main(int argc, char **argv) {
   }
 
   char pool[256];
+  enum dictionaries dict = UNKNOWN;
   int pool_size = 0;
 
-  if (argc > 3 && *argv[3] != '-' && *argv[3] != '+') {
+  if (argc > 3 && *argv[3] == '@') {
+    dict = parse_dict(argv[3] + 1);
+
+    if (dict == UNKNOWN) {
+      fprintf(stderr, "Неизвестный словарь: %s\n", argv[3] + 1);
+      exit(1);
+    }
+
+    pool_size = strlen(dicts[dict]);
+    memcpy(pool, dicts[dict], pool_size);
+  } else if (argc > 3 && *argv[3] != '-' && *argv[3] != '+') {
     char *src = argv[3];
     pool_size = strlen(src);
 
@@ -100,13 +129,13 @@ int main(int argc, char **argv) {
   }
 
   if (pool_size == 0) {
-
     fprintf(stderr, "Ошибка: количество доступных символов = 0\n");
     return 1;
   }
 
   unsigned char buffer[BUF_SIZE];
-  int limit = 256 - (256 % pool_size);
+  bool is_four = pool_size <= 16;
+  int limit = (is_four ? 16 : 256) - ((is_four ? 16 : 256) % pool_size);
   int bytes_read = 0;
   int current_byte = 0;
 
@@ -120,10 +149,20 @@ int main(int argc, char **argv) {
             perror("getrandom failed");
             exit(1);
           }
+
+          if (is_four)
+            bytes_read = bytes_read * 2;
           current_byte = 0;
         }
 
-        byte = buffer[current_byte++];
+        if (is_four) {
+          byte = buffer[current_byte >> 1];
+          byte = (byte >> ((1 - (current_byte & 1)) << 2)) & 0x0F;
+          current_byte++;
+        } else {
+          byte = buffer[current_byte++];
+        }
+
         if (byte < limit)
           break;
       }
